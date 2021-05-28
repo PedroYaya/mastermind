80d9db0e30cc6a5087f3f8032ebe7bd187348504
Pedro Yaya <pedro.y@floraqueen.com>
2021-05-28 11:54:31 +0200

    helper

--- src/components/Colors.vue
+++ src/components/Colors.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="colors">
-        <div v-for="(color, index) in colors" :key="index" :style="{background: color}" class="color"></div>
+        <div v-for="(color, index) in getCurrentGame.colors" :key="index"
+             :style="{background: color}"
+             class="color">
+        </div>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
+
     export default {
         name: 'Colors',
         computed: {
-            colors() {
-                return this.$store.getters.getCurrentGame.colors
-            }
+            ...mapGetters([
+                'getCurrentGame'
+            ])
         }
     }
 </script>
@@ -27,7 +32,6 @@
             width: 30px;
             height: 30px;
             border-radius: 30px;
-            background: green;
         }
     }
 </style>
