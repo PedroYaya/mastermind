bde549fafd3615c0e4f0f0fd606c00dc0ec442c2
Pedro Yaya <pedro.y@floraqueen.com>
2021-05-31 11:35:08 +0200

    pre served colors

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        currentGame: { "guesses": [], "num_colors": 4, "max_guesses": 8, "num_slots": 4},
+        currentGame: {"colors": ['red', 'blue', 'green', 'yellow'], "guesses": [], "num_colors": 4, "max_guesses": 8, "num_slots": 4},
         unitGuess: '',
         finalGuess: []
     },
