3e830f481c6e2f2b3ce67401f1fe9436361edf0e
Pedro Yaya <pedro.y@floraqueen.com>
2021-06-04 11:12:31 +0200

    a

--- tests/unit/Confirm.spec.js
+++ tests/unit/Confirm.spec.js
@@ -1,10 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Confirm from '@/components/buttons/Confirm.vue'
 
 describe('Confirm.vue', () => {
-    it('triggers click', () => {
-        const wrapper = shallowMount(Confirm)
-//test roto
-        expect(wrapper.trigger('confirmGuess')).toBe(!null)
+    it('triggers confirm event', async () => {
+        const wrapper = mount(Confirm)
+        await wrapper.trigger('click')
+
+        wrapper.vm.$emit('confirm', 0)
+        expect(wrapper.emitted().confirm).toBeTruthy()
     })
 })
--- tests/unit/New.spec.js
+++ tests/unit/New.spec.js
@@ -1,16 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import New from '@/components/buttons/New.vue'
 
 describe('New.vue', () => {
     it('renders props when passed', () => {
         const text = 'click here'
-        const wrapper = shallowMount(New, {
+        const wrapper = mount(New, {
             propsData: {
                 text: text
             }
         })
 
-        expect(wrapper.vm.$props.text).toBe(text)
+        expect(wrapper.text()).toContain(text)
         //expect(document.getElementsByClassName('new-game')[0].innerHTML).toBe((wrapper.vm.$props.text))
 
     })
