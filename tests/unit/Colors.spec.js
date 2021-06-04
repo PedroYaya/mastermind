726b41565b36589b35078a0969eddab464e4403f
Pedro Yaya <pedro.y@floraqueen.com>
2021-06-04 16:47:48 +0200

    a

---
+++ tests/unit/Colors.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Colors from '@/components/Colors.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('Colors.vue', () => {
+    let getters
+    let mutations
+    let store
+
+    beforeEach(() => {
+        getters = {
+            getCurrentGame() {
+                return {
+                    colors: ['red', 'yellow', 'blue', 'green']
+                }
+            }
+        }
+        mutations = {
+            setUnitGuess: jest.fn(),
+        }
+        store = new Vuex.Store({
+            getters,
+            mutations
+        })
+    })
+
+    it('calls store action "setUnitGuess" when button is clicked', () => {
+        const wrapper = shallowMount(Colors, { store, localVue })
+        wrapper.find('button').trigger('click')
+        expect(mutations.setUnitGuess).toHaveBeenCalled()
+    })
+})
